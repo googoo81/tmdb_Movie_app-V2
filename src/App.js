9d6a412274d0cb8228fadc6ab396672fcecd74cc
googoo81 <shw9052@naver.com>
2023-03-20 20:31:37 +0900

    라우터 연결 및 헤더 완성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Header from "./components/Header";
+import Celebrity from "./pages/Celebrity";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import NotFound from "./pages/NotFound";
+import Tv from "./pages/Tv"; 
+
 function App() {
   return (
-    <div className="App">
-
+    <div className="root-wrap">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie" element={<Movies />} />
+          <Route path="/tv" element={<Tv />} />
+          <Route path="/person" element={<Celebrity />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
